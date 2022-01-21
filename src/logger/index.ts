8454c5512e1bdb6cfe2e24ae990dea7c8375cc29
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-01-21 23:09:47 +0200

    add and configure logger 💻

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -2,13 +2,13 @@ import logger from "pino";
 import dayjs from "dayjs";
 
 const _logger = logger({
-    transport: {
-        target: 'pino-pretty'
-    },
-    base: {
-        pid: false,
-    },
-    timestamp: () => `,"time":"${dayjs().format()}"`,
+  transport: {
+    target: "pino-pretty",
+  },
+  base: {
+    pid: false,
+  },
+  timestamp: () => `,"time":"${dayjs().format()}"`,
 });
 
 export default _logger;
