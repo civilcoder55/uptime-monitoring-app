739be0a7cb0586ae9b147a76535de2b01fca15af
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-01-25 22:41:23 +0200

    clean check serive

--- src/services/check.service.ts
+++ src/services/check.service.ts
@@ -1,25 +1,19 @@
 import { CheckDocument } from "../types/check.type";
-import CheckModel from "../models/check.model";
+import checkModel from "../models/check.model";
 
-export async function createCheck(userId: string, checkData: CheckDocument): Promise<CheckDocument> {
-  // double check for correct protocol
-  if (checkData.protocol !== "tcp") {
-    checkData.protocol = checkData.url.startsWith("https") ? "https" : "http";
-  }
-  checkData.user = userId;
-
-  return await CheckModel.create(checkData);
+export async function createCheck(checkData: CheckDocument): Promise<CheckDocument> {
+  return await checkModel.create(checkData);
 }
 
 export async function getChecks(userId: string, tags: string | string[]): Promise<CheckDocument[]> {
   if (tags) {
-    return await CheckModel.find({ user: userId, tags: { $all: tags } });
+    return await checkModel.find({ user: userId, tags: { $all: tags } });
   }
-  return await CheckModel.find({ user: userId });
+  return await checkModel.find({ user: userId });
 }
 
 export async function getCheck(userId: string, checkId: string): Promise<CheckDocument> {
-  const check = await CheckModel.findOne({ _id: checkId, user: userId });
+  const check = await checkModel.findOne({ _id: checkId, user: userId });
 
   if (!check) {
     throw {
@@ -32,17 +26,11 @@ export async function getCheck(userId: string, checkId: string): Promise<CheckDo
 
 export async function updateCheck(userId: string, checkId: string, checkData: CheckDocument): Promise<CheckDocument> {
   const check = await getCheck(userId, checkId);
-  // double check for correct protocol
-  if (checkData.protocol !== "tcp") {
-    checkData.protocol = checkData.url.startsWith("https") ? "https" : "http";
-  }
-
   Object.assign(check, checkData);
   return await check.save();
 }
 
 export async function deleteCheck(userId: string, checkId: string): Promise<void> {
   const check = await getCheck(userId, checkId);
-
   await check.remove();
 }
