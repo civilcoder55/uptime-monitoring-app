6bdc94e7a73c1d576d7a8eac8d7479170538d46d
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-01-25 22:40:20 +0200

    edit check schema

--- src/schemas/check.schema.ts
+++ src/schemas/check.schema.ts
@@ -10,8 +10,12 @@ export const createCheckSchema = object({
     ),
     tags: array().of(string()).typeError("tags field must be array of strings"),
     ignoreSSL: boolean().typeError("ignoreSSL field must be boolean"),
-    timeout: number().typeError("timeout field must be number").min(5, "timeout must be greater than or equal 5 seconds"),
-    interval: number().typeError("interval field must be number").min(60, "interval must be greater than or equal 60 seconds"),
+    timeout: number()
+      .typeError("timeout field must be number")
+      .min(5, "timeout must be greater than or equal 5 seconds"),
+    interval: number()
+      .typeError("interval field must be number")
+      .min(60, "interval must be greater than or equal 60 seconds"),
     threshold: number().typeError("threshold field must be number").min(1, "threshold must be greater than or equal 1"),
     webhook: string().matches(
       /(http(s)?:\/\/)(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
@@ -19,20 +23,28 @@ export const createCheckSchema = object({
     ),
 
     path: string().typeError("path field must be string"),
-    url: string()
-      .required("url field is required")
+    port: number()
+      .typeError("port field must be number")
+      .min(0, "port must be greater than or equal 0 ")
+      .max(65535, "port must be less than or equal 65535")
+      .when("protocol", {
+        is: (protocol: string) => protocol === "tcp",
+        then: number().required(),
+      }),
+    host: string()
+      .required("host field is required")
       .when("protocol", {
         is: (protocol: string) => protocol === "tcp",
         then: string().matches(
           /^(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))$/,
-          "url field must be correct ipv4 !"
+          "host field must be correct ipv4 !"
         ),
       })
       .when("protocol", {
         is: (protocol: string) => protocol !== "tcp",
         then: string().matches(
           /(http(s)?:\/\/)(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
-          "url field must be correct url !"
+          "host field must be correct url !"
         ),
       }),
     protocol: mixed().required("protocol field is required").oneOf(["http", "https", "tcp"]),
