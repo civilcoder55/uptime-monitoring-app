22d2d69767b795b110a15d3ec2bcf0884b06ef69
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-01-25 19:25:06 +0200

    update database to use config

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 import logger from "../logger";
-
-const MONGO_URI = process.env.MONGO_URI as string;
+import config from "../config";
 
 export const connect = async () => {
   return mongoose
-    .connect(MONGO_URI)
+    .connect(config.MONGO_URI)
     .then(() => {
       logger.info("[*] Mongo DB connected successfully.");
     })
     .catch((error) => {
-      logger.error(error,"[x] Mongo DB connection failed.");
+      logger.error(error, "[x] Mongo DB connection failed.");
       process.exit(1);
     });
 };
